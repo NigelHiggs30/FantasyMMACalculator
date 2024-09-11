34dff981331931e035a965abb6181e618aed3411
NigelHiggs30 <higgsn@oregonstate.edu>
2024-09-11 13:42:59 -0700

    Added fight_points_evaluator.py and rewrote main.py

--- main.py
+++ main.py
@@ -1,113 +1,28 @@
 from modules.scraper import get_fight_data
 from modules.utils import convert_time_to_seconds
 from modules.fight_link import url_links
+from modules.fight_points_evaluator import evaluate_fight_points
 from modules.parser import WebPageParser
 from modules.calculator import FantasyPointsCalculator
+
 from bs4 import BeautifulSoup
 import requests
 import argparse
 
-def main(url):
-     ##url of specific fight.
-    # url = "http://ufcstats.com/fight-details/4047e98132306cd5"
-    website_data = get_fight_data(url)
-    # print(website_data)
-
-    #parsing logic for the variables of interest.
-    parser_html = WebPageParser(website_data)
-
-     # Extract fight details
-    fight_results_dic, fighter_1_name, fighter_2_name = parser_html.winner_looser()
-    fight_method, fight_round, fight_time, fight_format = parser_html.extract_fight_details()
-    
-    # Extract scoring moves
-    fight_data = parser_html.extract_scoring_moves()
-
-    # Initialize the FantasyPointsCalculator with the parsed fight details
-    calculator = FantasyPointsCalculator(
-        fight_results_dic, 
-        fighter_1_name, 
-        fighter_2_name, 
-        fight_method, 
-        fight_round, 
-        fight_time, 
-        fight_format
-    )
-
-    fighter_1_score, fighter_2_score = calculator.calculate_points(*fight_data)
-
-    # Determine the winner
-    winner = None
-    if fight_results_dic[fighter_1_name] == "W":
-        winner = fighter_1_name
-    elif fight_results_dic[fighter_2_name] == "W":
-        winner = fighter_2_name
-
-    if fight_round == 1:
-        #assign points
-        if winner == fighter_1_name:
-                fighter_1_score += 90
-        elif winner == fighter_2_name:
-                fighter_2_score += 90  # type: ignore
-        # check if fight ended 60 seconds in
-        temp = convert_time_to_seconds(fight_time)
-        if int(temp) <=60:
-            #bonus points.
-            if winner == fighter_1_name:
-                fighter_1_score += 25
-            elif winner == fighter_2_name:
-                fighter_2_score += 25  # type: ignore
-
-    if fight_round == 2:
-            #assign points
-        if winner == fighter_1_name:
-                fighter_1_score += 70
-        elif winner == fighter_2_name:
-                fighter_2_score += 70  # type: ignore
-
-    if fight_round == 3:
-        #assign points
-        if fight_method == "Decision - Unanimous" or fight_method == "Decision - Split":
-            if winner == fighter_1_name:
-                    fighter_1_score += 30
-            elif winner == fighter_2_name:
-                    fighter_2_score += 30  # type: ignore
-        else:
-            if winner == fighter_1_name:
-                    fighter_1_score += 45
-            elif winner == fighter_2_name:
-                    fighter_2_score += 45  # type: ignore
-
-    if fight_round == 4:
-            #assign points
-        if winner == fighter_1_name:
-                fighter_1_score += 40
-        elif winner == fighter_2_name:
-                fighter_2_score += 40  # type: ignore
-
-    if fight_round == 5:
-        #assign points
-        if fight_method == "Decision - Unanimous" or fight_method == "Decision - Split":
-            if winner == fighter_1_name:
-                    fighter_1_score += 30
-            elif winner == fighter_2_name:
-                    fighter_2_score += 30  # type: ignore
-                    
-        if winner == fighter_1_name:
-                fighter_1_score += 40
-        elif winner == fighter_2_name:
-                fighter_2_score += 40  # type: ignore
-
-    print(f"{fighter_1_name}: {fighter_1_score}")
-    print(f"{fighter_2_name}: {fighter_2_score}\n")
-    
 if __name__ == "__main__":
-    
+       
     parser = argparse.ArgumentParser(description="Scrape UFC fighter details.")
+    
     parser.add_argument(
         "url",
         type=str,
         help="URL of the UFC fighter details page."
+    )
+    
+    parser.add_argument(
+    "--all",
+    action="store_true",
+    help="Process all fight links from the page."
     )
 
     args = parser.parse_args()
@@ -119,5 +34,10 @@ if __name__ == "__main__":
     # Check fight_link
     fighters_links = url_links(soup)
 
-    for x in range(len(fighters_links)):
-        main(fighters_links[x])
+    if args.all:
+        # Loop through all fighter links if --all is passed
+        for fighter_link in fighters_links:
+            evaluate_fight_points(fighter_link)
+    else:
+        # Process only the first fighter link
+        evaluate_fight_points(fighters_links[0])
---
+++ modules/fight_points_evaluator.py
@@ -0,0 +1,100 @@
+from modules.scraper import get_fight_data
+from modules.parser import WebPageParser
+from modules.calculator import FantasyPointsCalculator
+from modules.utils import convert_time_to_seconds
+
+
+
+def evaluate_fight_points(url):
+     ##url of specific fight.
+    # url = "http://ufcstats.com/fight-details/4047e98132306cd5"
+    website_data = get_fight_data(url)
+    # print(website_data)
+
+    #parsing logic for the variables of interest.
+    parser_html = WebPageParser(website_data)
+
+     # Extract fight details
+    fight_results_dic, fighter_1_name, fighter_2_name = parser_html.winner_looser()
+    fight_method, fight_round, fight_time, fight_format = parser_html.extract_fight_details()
+    
+    # Extract scoring moves
+    fight_data = parser_html.extract_scoring_moves()
+
+    # Initialize the FantasyPointsCalculator with the parsed fight details
+    calculator = FantasyPointsCalculator(
+        fight_results_dic, 
+        fighter_1_name, 
+        fighter_2_name, 
+        fight_method, 
+        fight_round, 
+        fight_time, 
+        fight_format
+    )
+
+    fighter_1_score, fighter_2_score = calculator.calculate_points(*fight_data)
+
+    # Determine the winner
+    winner = None
+    if fight_results_dic[fighter_1_name] == "W":
+        winner = fighter_1_name
+    elif fight_results_dic[fighter_2_name] == "W":
+        winner = fighter_2_name
+
+    if fight_round == 1:
+        #assign points
+        if winner == fighter_1_name:
+                fighter_1_score += 90
+        elif winner == fighter_2_name:
+                fighter_2_score += 90  # type: ignore
+        # check if fight ended 60 seconds in
+        temp = convert_time_to_seconds(fight_time)
+        if int(temp) <=60:
+            #bonus points.
+            if winner == fighter_1_name:
+                fighter_1_score += 25
+            elif winner == fighter_2_name:
+                fighter_2_score += 25  # type: ignore
+
+    if fight_round == 2:
+            #assign points
+        if winner == fighter_1_name:
+                fighter_1_score += 70
+        elif winner == fighter_2_name:
+                fighter_2_score += 70  # type: ignore
+
+    if fight_round == 3:
+        #assign points
+        if fight_method == "Decision - Unanimous" or fight_method == "Decision - Split":
+            if winner == fighter_1_name:
+                    fighter_1_score += 30
+            elif winner == fighter_2_name:
+                    fighter_2_score += 30  # type: ignore
+        else:
+            if winner == fighter_1_name:
+                    fighter_1_score += 45
+            elif winner == fighter_2_name:
+                    fighter_2_score += 45  # type: ignore
+
+    if fight_round == 4:
+            #assign points
+        if winner == fighter_1_name:
+                fighter_1_score += 40
+        elif winner == fighter_2_name:
+                fighter_2_score += 40  # type: ignore
+
+    if fight_round == 5:
+        #assign points
+        if fight_method == "Decision - Unanimous" or fight_method == "Decision - Split":
+            if winner == fighter_1_name:
+                    fighter_1_score += 30
+            elif winner == fighter_2_name:
+                    fighter_2_score += 30  # type: ignore
+                    
+        if winner == fighter_1_name:
+                fighter_1_score += 40
+        elif winner == fighter_2_name:
+                fighter_2_score += 40  # type: ignore
+
+    print(f"{fighter_1_name}: {fighter_1_score}")
+    print(f"{fighter_2_name}: {fighter_2_score}\n")
